746844727ee47cdb2d19f1c2a559b44b6e580e84
Karolina <karolina.limanowska@gmail.com>
2024-08-19 20:59:54 +0200

    setting a folder structure and creating example routes

--- app/map/page.js
+++ app/map/page.js
@@ -1,9 +1,9 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function Map() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    Main page here
+      <h1>Map comes here</h1>
     </main>
   );
 }
--- app/place/page.js
+++ app/place/page.js
@@ -1,9 +1,9 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function Place() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    Main page here
+      <h1>Place 1 description</h1>
     </main>
   );
 }
