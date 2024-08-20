e5b72642fb9a5b8cfa97fc0f583b349861543615
Karolina <karolina.limanowska@gmail.com>
2024-08-20 14:00:53 +0200

    adding a route for timetable

---
+++ components/Menu.js
@@ -0,0 +1,18 @@
+'use client'
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function Menu () {
+  const pathname = usePathname();
+
+  return (
+    
+      <div className="flex justify-end bg-black/[0.6] fixed right-0 top-0 z-10 space-x-16 mx-auto w-full p-4">
+        <div className="text-base text-white"><Link className={`link ${pathname === '/' ? 'active' : ''}`} href="/">Home</Link></div>
+        <div className="text-base text-white"><Link className={`link ${pathname === '/map' ? 'active' : ''}`} href="/map">Map</Link></div>
+        <div className="text-base text-white"><Link className={`link ${pathname === '/timetable' ? 'active' : ''}`} href="/timetable">Time</Link></div>
+      </div>
+  );
+
+}
