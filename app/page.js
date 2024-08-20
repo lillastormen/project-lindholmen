3132321e054bee42267ad6c583cc93db845380df
Karolina <karolina.limanowska@gmail.com>
2024-08-20 14:33:41 +0200

    merging

--- app/page.js
+++ app/page.js
@@ -4,14 +4,15 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
+
 export default function Home() {
   const pathname = usePathname();
-  
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
     Main page here
     <div className="text-base text-white"><Link className={`link ${pathname === '/map' ? 'active' : ''}`} href="/map">Map</Link></div>
-        <div className="text-base text-white"><Link className={`link ${pathname === '/timetable' ? 'active' : ''}`} href="/timetable">Time</Link></div>
+    <div className="text-base text-white"><Link className={`link ${pathname === '/timetable' ? 'active' : ''}`} href="/timetable">Time</Link></div>
     </main>
   );
 }
