1986a8f9793fee8d5278ffa1f8cbb24b0335050a
Karolina <karolina.limanowska@gmail.com>
2024-08-20 14:06:13 +0200

    working on a structure

--- components/Header.js
+++ components/Header.js
@@ -8,7 +8,6 @@ export default function Header () {
   const pathname = usePathname();
 
   return (
-
       <div className="flex justify-end bg-black/[0.6] fixed right-0 top-0 z-10 space-x-16 mx-auto w-full p-4">
        <Menu />
       </div>
