33116584aefd3d2d9f9d070734b61083e02588a7
Karolina <karolina.limanowska@gmail.com>
2024-08-21 11:41:36 +0200

    changing styling

--- components/Header.js
+++ components/Header.js
@@ -7,14 +7,11 @@ import { usePathname } from "next/navigation";
 export default function Header() {
   const pathname = usePathname();
   return (
-
-
     <div
       className={`absolute  right-0 top-0 z-10 w-full p-4 ${
         pathname === "/" ? "bg-black/[0.6]" : ""
       }`}
     >
-
       <Menu />
     </div>
   );
